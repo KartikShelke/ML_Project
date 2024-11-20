dcda68480c1d87bd0d9619efeb94c71d8e87935e
Kartik Shelke <kartikshelke2201@gmail.com>
2024-11-21 02:36:39 +0530

    app.py

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ model = pickle.load(open('model.pkl', 'rb'))
 # Streamlit App
 st.title("Credit Default Prediction")
 
+# Add background image using CSS
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://media.licdn.com/dms/image/D4D12AQE5tMSbHzL5Yw/article-cover_image-shrink_600_2000/0/1686909157848?e=2147483647&v=beta&t=QAErDuhf8HDQ3ojR9z9ajoGCCf3hpiF4wS-dQrsEc3Q');
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Inputs from user
 income = st.number_input("Income", min_value=0, max_value=1000000, value=10000)
 age = st.number_input("Age", min_value=18, max_value=100, value=30)
@@ -21,12 +34,11 @@ loan_to_income = loan / income if income != 0 else 0
 # Ensure column names are consistent with what the model expects
 input_data = pd.DataFrame([[income, age, loan, loan_to_income]], columns=['Income', 'Age', 'Loan', 'Loan to Income'])
 
-
 # Prediction button
 if st.button("Predict Default"):
     # Make the prediction
     prediction = model.predict(input_data)
-    
+
     # Output
     if prediction == 0:
         st.write("Prediction: **No Default**")
